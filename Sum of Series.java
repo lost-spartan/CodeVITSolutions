b7de2422449a3f54ab12daf3f839c06073cffab3
Kushagra Mani <80683602+lost-spartan@users.noreply.github.com>
2023-01-21 16:30:53 +0530

    Update Sum of Series.java

--- Sum of Series.java
+++ Sum of Series.java
@@ -1,22 +1,24 @@
 // You are using Java
 import java.util.*;
 import java.lang.Math;
+import java.text.DecimalFormat;
 
 class Solution{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         Solution s = new Solution();
+        DecimalFormat df = new DecimalFormat("0.00");
         int val = sc.nextInt();
         int term = sc.nextInt();
-        int sum = 0;
+        double sum = 0;
         double a;
         
         for(int  i=0; i<term; i++){
-            a = (Math.pow(-1,i) + Math.pow(val,i*2))/s.Factorial(i*2);
-            sum = sum + (int)a;
+            a = (Math.pow(-1,i) * Math.pow(val,i*2))/s.Factorial(i*2);
+            sum = sum + a;
         }
         
-        System.out.println(sum);
+        System.out.println(df.format(sum));
     }
     static int Factorial(int a){
         int fact = 1;
