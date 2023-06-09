e848b0d3f62bb4cc7d8b43f6b4ca39e1933d8afd
Kushagra Mani <80683602+lost-spartan@users.noreply.github.com>
2023-06-09 15:47:23 +0530

    To reverse LinkedList

--- Linkedlist/Reverse.java
+++ Linkedlist/Reverse.java
@@ -1,3 +1,20 @@
 class Solution{
-
+  public static ListNode Reverse(ListNode node)
+    {
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        node = prev;
+        return node;
+    }
 }
+
+// By using Java Collection Class in java.util.LinkedLinst
+LinkedList<Integer> list = new LinkedList<Integer>();
+Collections.reverse(list)
