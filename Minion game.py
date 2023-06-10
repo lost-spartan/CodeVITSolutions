56068074eab5088f9838319d0ef3668226b3cd54
Kushagra Mani <80683602+lost-spartan@users.noreply.github.com>
2023-06-10 12:10:14 +0530

    Count the substring possible starting and not starting from a vowel
    Kevin and Stuart want to play the 'The Minion Game'.
    
    Game Rules
    
    Both players are given the same string, .
    Both players have to make substrings using the letters of the string .
    Stuart has to make words starting with consonants.
    Kevin has to make words starting with vowels.
    The game ends when both players have made all possible substrings.
    
    Scoring
    A player gets +1 point for each occurrence of the substring in the string .
    
    For Example:
    String  = BANANA
    Kevin's vowel beginning word = ANA
    Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
    
    RESULT FOR KEVIN AND STUART MIGHT HAVE GOT INTERCHANGED

--- Minion game.py
+++ Minion game.py
@@ -1,15 +1,14 @@
 def minion_game(s):
-    s1=0
-    s2=0
-    vow='AEIOU'
+    vow = 0
+    con = 0
     for i in range(len(s)):
-        if s[i] not in vow:
-            s1=s1+(len(s)-i)
+        if s[i] in 'AEIOU':
+            vow = vow +(len(s)-i)
         else:
-            s2=s2+(len(s)-i)   
-    if s1>s2:
+            con = con +(len(s)-i)   
+    if vow > con:
         print("Stuart",s1)
-    elif s2>s1:
+    elif con > vow:
         print("Kevin",s2)
     else:
         print("Draw")    
