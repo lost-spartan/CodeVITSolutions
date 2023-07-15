c6073f679f38cdd88b4ed4a31ba2c477f872cb26
Kushagra Mani <80683602+lost-spartan@users.noreply.github.com>
2023-07-15 15:16:25 +0530

    Update Zig-Zag.java

--- Matrix/Zig-Zag.java
+++ Matrix/Zig-Zag.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 } { 7, 8, 9} };
+		int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9} };
 		int n = arr.length;
 		System.out.println(result(arr,n));
 	}
